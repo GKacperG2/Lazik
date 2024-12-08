24a988f6d161ee5b447f085480f76e05fd479a18
TakieCos <kacperapptak@gmail.com>
2024-12-08 23:46:24 +0100

    Poprawa generowania traktoru

--- Gl_template.c
+++ Gl_template.c
@@ -190,114 +190,198 @@ float getTerrainHeight(float x, float z) {
 	return -5.0f + sinf(x * 0.05f) * cosf(z * 0.05f) * 0.5f;
 }
 
-// Deklaracje
-void prostopadloscian(double a, double b, double offsetX, double offsetY, double offsetZ);
-void prostopadloscian1(double a, double b, double offsetX, double offsetY, double offsetZ);
-void felga(double r, double h, double offsetX, double offsetY, double offsetZ);
-void walec(double r, double h, double offsetX, double offsetY, double offsetZ);
-
 void prostopadloscian(double a, double b, double offsetX, double offsetY, double offsetZ) {
-	// Bez zmian, dodaæ sufixy f do liczb sta³ych:
-	for (float z = 0.0f; z <= 20.0f; z += 0.1f) {
+	for (float z = 0; z <= 20; z += 0.1) {
 		glBegin(GL_TRIANGLE_STRIP);
-		for (float x = -10.0f; x <= 95.0f; x += 0.1f) {
-			glColor3f(0.8f, 0.0f, 1.0f);
-			glVertex3f(x + (float)offsetX, (float)offsetY, z + (float)offsetZ);
-			glVertex3f(x + (float)offsetX, (float)offsetY, z + 0.1f + (float)offsetZ);
+		for (float x = -10; x <= 20 + 75; x += 0.1) {
+			glColor3f(0.8, 0.0, 1);
+			glVertex3d(x + offsetX, 0 + offsetY, z + offsetZ);
+			glVertex3d(x + offsetX, 0 + offsetY, z + 0.1 + offsetZ);
 		}
 		glEnd();
 	}
 
-	// Pozosta³e pêtle analogicznie - wszystkie wartoœci sta³e z sufiksem f
-	// Dla czytelnoœci pominiêto zmianê wszystkich sta³ych, ale nale¿y dodaæ 'f'
-	// do wszystkich liczb zmiennoprzecinkowych. 
-	// Ostrze¿enia o konwersji double->float dotycz¹ g³ównie literali liczbowych.
-	// Po prostu dodaj 'f' do wszystkich sta³ych typu np. 0.1, 20, 0 itd.
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0, 1.0, 0.0);
+		for (float x = -10; x <= 20 + 75; x += 0.1) {
+			glVertex3d(x + offsetX, z + offsetY, 0 + offsetZ);
+			glVertex3d(x + offsetX, z + 0.1 + offsetY, 0 + offsetZ);
+		}
+		glEnd();
+	}
 
-	// ... Kontynuacja jak w poprzednim kodzie ...
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3d(0.8, 0.0, 0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(-10 + offsetX, z + offsetY, x + offsetZ);
+			glVertex3d(-10 + offsetX, z + 0.1 + offsetY, x + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0, 0.0, 0.0);
+		for (float x = -10; x <= 20 + 75; x += 0.1) {
+			glVertex3d(x + offsetX, 20 + offsetY, z + offsetZ);
+			glVertex3d(x + offsetX, 20 + offsetY, z + 0.1 + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float x = -10; x <= 20 + 75; x += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(0.0, 1.0, 0.0);
+		for (float y = 0; y <= 20; y += 0.1) {
+			glVertex3d(x + offsetX, y + offsetY, 20 + offsetZ);
+			glVertex3d(x + 0.1 + offsetX, y + offsetY, 20 + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3d(0.8, 0.0, 0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(95 + offsetX, z + offsetY, x + offsetZ);
+			glVertex3d(95 + offsetX, z + 0.1 + offsetY, x + offsetZ);
+		}
+		glEnd();
+	}
 }
-
 void prostopadloscian1(double a, double b, double offsetX, double offsetY, double offsetZ) {
-	// Analogicznie dodaæ f do sta³ych
-	for (float z = 0.0f; z <= 20.0f; z += 0.1f) {
+	for (float z = 0; z <= 20; z += 0.1) {
 		glBegin(GL_TRIANGLE_STRIP);
-		for (float x = 0.0f; x <= 20.0f; x += 0.1f) {
-			glColor3f(0.8f, 0.0f, 1.0f);
-			glVertex3f(x + (float)offsetX, (float)offsetY, z + (float)offsetZ);
-			glVertex3f(x + (float)offsetX, (float)offsetY, z + 0.1f + (float)offsetZ);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glColor3f(0.8, 0.0, 1);
+			glVertex3d(x + offsetX, 0 + offsetY, z + offsetZ);
+			glVertex3d(x + offsetX, 0 + offsetY, z + 0.1 + offsetZ);
 		}
 		glEnd();
 	}
 
-	// Analogicznie jak wy¿ej w pozosta³ych pêtlach
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0, 1.0, 0.0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(x + offsetX, z + offsetY, 0 + offsetZ);
+			glVertex3d(x + offsetX, z + 0.1 + offsetY, 0 + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3d(0.8, 0.0, 0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(0 + offsetX, z + offsetY, x + offsetZ);
+			glVertex3d(0 + offsetX, z + 0.1 + offsetY, x + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0, 0.0, 0.0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(x + offsetX, 20 + offsetY, z + offsetZ);
+			glVertex3d(x + offsetX, 20 + offsetY, z + 0.1 + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float x = 0; x <= 20; x += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(0.0, 1.0, 0.0);
+		for (float y = 0; y <= 20; y += 0.1) {
+			glVertex3d(x + offsetX, y + offsetY, 20 + offsetZ);
+			glVertex3d(x + 0.1 + offsetX, y + offsetY, 20 + offsetZ);
+		}
+		glEnd();
+	}
+
+	for (float z = 0; z <= 20; z += 0.1) {
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3d(0.8, 0.0, 0);
+		for (float x = 0; x <= 20; x += 0.1) {
+			glVertex3d(20 + offsetX, z + offsetY, x + offsetZ);
+			glVertex3d(20 + offsetX, z + 0.1 + offsetY, x + offsetZ);
+		}
+		glEnd();
+	}
 }
-
 void felga(double r, double h, double offsetX, double offsetY, double offsetZ) {
-	const float PI = 3.14f;
-	double alpha;
-	double x, y;
+	const double PI = 3.14;
+	double x, y, alpha;
 
+	// Podstawa dolna
 	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.8f, 1.0f, 0.0f);
-	glVertex3f((float)offsetX, (float)offsetY, (float)offsetZ);
-	for (alpha = 0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	glColor3d(0.8, 1.0, 0.0);
+	glVertex3d(offsetX, offsetY, offsetZ); // Œrodek podstawy dolnej
+	for (alpha = 0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)offsetZ);
+		glVertex3d(x, y, offsetZ);
 	}
 	glEnd();
 
+	// Œciany boczne
 	glBegin(GL_QUAD_STRIP);
-	for (alpha = 0.0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)offsetZ);
-		glVertex3f((float)x, (float)y, (float)(offsetZ + h));
+		glVertex3d(x, y, offsetZ);         // Punkt na dolnej podstawie
+		glVertex3d(x, y, offsetZ + h);     // Punkt na górnej podstawie
 	}
 	glEnd();
 
+	// Podstawa górna
 	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.8f, 1.0f, 0.0f);
-	glVertex3f((float)offsetX, (float)offsetY, (float)(offsetZ + h));
-	for (alpha = 0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	glColor3d(0.8, 1.0, 0.0);
+	glVertex3d(offsetX, offsetY, offsetZ + h); // Œrodek podstawy górnej
+	for (alpha = 0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)(offsetZ + h));
+		glVertex3d(x, y, offsetZ + h);
 	}
 	glEnd();
 }
 
 void walec(double r, double h, double offsetX, double offsetY, double offsetZ) {
-	const float PI = 3.14f;
-	double alpha;
-	double x, y;
+	const double PI = 3.14;
+	double x, y, alpha;
 
+	// Podstawa dolna
 	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.8f, 0.0f, 0.0f);
-	glVertex3f((float)offsetX, (float)offsetY, (float)offsetZ);
-	for (alpha = 0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	glColor3d(0.8, 0.0, 0.0);
+	glVertex3d(offsetX, offsetY, offsetZ); // Œrodek podstawy dolnej
+	for (alpha = 0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)offsetZ);
+		glVertex3d(x, y, offsetZ);
 	}
 	glEnd();
 
+	// Œciany boczne
 	glBegin(GL_QUAD_STRIP);
-	for (alpha = 0.0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	for (alpha = 0.0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)offsetZ);
-		glVertex3f((float)x, (float)y, (float)(offsetZ + h));
+		glVertex3d(x, y, offsetZ);         // Punkt na dolnej podstawie
+		glVertex3d(x, y, offsetZ + h);     // Punkt na górnej podstawie
 	}
 	glEnd();
 
+	// Podstawa górna
 	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(0.8f, 0.0f, 0.0f);
-	glVertex3f((float)offsetX, (float)offsetY, (float)(offsetZ + h));
-	for (alpha = 0; alpha <= 2 * M_PI; alpha += M_PI / 128.0) {
+	glColor3d(0.8, 0.0, 0.0);
+	glVertex3d(offsetX, offsetY, offsetZ + h); // Œrodek podstawy górnej
+	for (alpha = 0; alpha <= 2 * PI; alpha += PI / 128.0) {
 		x = r * sin(alpha) + offsetX;
 		y = r * cos(alpha) + offsetY;
-		glVertex3f((float)x, (float)y, (float)(offsetZ + h));
+		glVertex3d(x, y, offsetZ + h);
 	}
 	glEnd();
 }
